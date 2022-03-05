c08b03b9c419d87164894bc8020c67c109cf6983
Mojtaba Sohrabi <mojtaba_sohrabi_1377@yahoo.com>
2022-03-05 12:35:54 +0330

    create models

--- yektanet/advertiser_management/models.py
+++ yektanet/advertiser_management/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Advertiser(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    clicks = models.PositiveIntegerField(default=0)
+    views = models.PositiveIntegerField(default=0)
+
+
+class Ad(models.Model):
+    id = models.AutoField(primary_key=True)
+    #advertiser_id = models.ForeignKey(Advertiser, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    image_url = models.CharField(max_length=255)
+    link = models.TextField()
+    clicks = models.PositiveIntegerField(default=0)
+    views = models.PositiveIntegerField(default=0)
+    approve = models.BooleanField(default=False)
