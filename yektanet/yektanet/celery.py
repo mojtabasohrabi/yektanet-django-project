d784718eb7c077ceb7bb6c3000e93665ada60f1e
Mojtaba Sohrabi <mojtaba_sohrabi_1377@yahoo.com>
2022-03-12 11:51:42 +0330

    add CELERY_BROKER_URL in settings.py

--- yektanet/yektanet/celery.py
+++ yektanet/yektanet/celery.py
@@ -2,10 +2,7 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
-# setting the Django settings module.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'celery_task.settings')
 app = Celery('celery_task')
 app.config_from_object('django.conf:settings', namespace='CELERY')
-
-# Looks up for task modules in Django applications and loads them
 app.autodiscover_tasks()
