fbf76131f4279dcffb48d891d50eca2aee56fe15
Mojtaba Sohrabi <mojtaba_sohrabi_1377@yahoo.com>
2022-03-12 12:32:52 +0330

    add rest framework in settings.py

--- yektanet/yektanet/settings.py
+++ yektanet/yektanet/settings.py
@@ -72,6 +72,13 @@ TEMPLATES = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
+
 WSGI_APPLICATION = 'yektanet.wsgi.application'
 
 # Database
