9e2862c58fc15a36e650fa02f197c0f54433823b
Mojtaba Sohrabi <mojtaba_sohrabi_1377@yahoo.com>
2022-03-13 17:36:39 +0330

    change tasks codes

--- advertiser_management/task.py
+++ advertiser_management/task.py
@@ -10,12 +10,12 @@ app = Celery()
 
 @app.task
 def calculate_report_hourly():
-    views = View.objects.annotate(date=TruncHour('viewed_date')).values('date', 'ad') \
-        .annotate(count=Count('ad')).values('ad', 'viewed_date', 'count').filter(
-        viewed_date__hour=datetime.now().hour)
-    clicks = Click.objects.annotate(date=TruncHour('clicked_date')).values('clicked_date', 'ad') \
-        .annotate(count=Count('ad')).values('ad', 'clicked_date', 'count').filter(
-        clicked_date__hour=datetime.now().hour)
+    views = View.objects.filter(
+        viewed_date__hour=datetime.now().hour).annotate(date=TruncHour('created')).values('created', 'ad') \
+        .annotate(count=Count('ad')).values('ad', 'created', 'count')
+    clicks = Click.objects.filter(
+        clicked_date__hour=datetime.now().hour).annotate(date=TruncHour('created')).values('created', 'ad') \
+        .annotate(count=Count('ad')).values('ad', 'created', 'count')
     for ad in Ad.objects.values_list('pk'):
         ReportModel.objects.create(ad=ad, clicks_count=clicks.get(ad_id=ad, default=0),
                                    views_count=views.get(ad_id=ad),
@@ -24,12 +24,12 @@ def calculate_report_hourly():
 
 @app.task
 def calculate_report_daily():
-    views = View.objects.annotate(date=TruncHour('viewed_date')).values('date', 'ad').annotate(count=Count('ad')) \
-        .values('ad', 'viewed_date', 'count').filter(
-        viewed_date__hour=datetime.now().day)
-    clicks = Click.objects.annotate(date=TruncHour('clicked_date')).values('clicked_date', 'ad').annotate(
-        count=Count('ad')).values('ad', 'clicked_date', 'count').filter(
-        clicked_date__hour=datetime.now().day)
+    views = View.objects.filter(
+        viewed_date__hour=datetime.now().day).values('created', 'ad').annotate(date=TruncHour('created')).annotate(count=Count('ad')) \
+        .values('ad', 'created', 'count')
+    clicks = Click.objects.filter(
+        viewed_date__hour=datetime.now().day).values('created', 'ad').annotate(date=TruncHour('created')).annotate(
+        count=Count('ad')).values('ad', 'created', 'count')
     for ad in Ad.objects.values_list('pk'):
         ReportModel.objects.create(ad=ad, clicks_count=clicks.get(ad_id=ad, default=0),
                                    views_count=views.get(ad_id=ad),
